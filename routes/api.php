08fb8d31a6ea92baff216e8ef7c99fad4b9f7f90
amiralii <mr.roshanae@gmail.com>
2018-11-09 13:44:15 +0330

    fixed

--- routes/api.php
+++ routes/api.php
@@ -1,2 +1,12 @@
 <?php
 
+
+/*
+|--------------------------------------------------------------------------
+| Amirali Roshanaei - mr.roshanae@gmail.com
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::post('/{botID}/webhook', '\App\Http\Aggregates\Start\Controller\StartController@init');
+
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,5 @@
 <?php
 
 
-/*
-|--------------------------------------------------------------------------
-| Amirali Roshanaei - mr.roshanae@gmail.com
-|--------------------------------------------------------------------------
-|
-*/
-
-Route::post('/{botID}/webhook', '\App\Http\Aggregates\Start\Controller\StartController@init');
-
 
 
