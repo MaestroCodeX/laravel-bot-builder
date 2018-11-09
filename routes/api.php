c946ca264aee01924cb40e1280b3e47bbd9188eb
amiralii <mr.roshanae@gmail.com>
2018-11-09 13:21:50 +0330

    api fixed

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,2 @@
 <?php
 
-
-/*
-|--------------------------------------------------------------------------
-| Amirali Roshanaei - mr.roshanae@gmail.com
-|--------------------------------------------------------------------------
-|
-*/
-
-Route::post('/{botID}/webhook', '\App\Http\Aggregates\Start\Controller\StartController@init');
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
+
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Amirali Roshanaei - mr.roshanae@gmail.com
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/{botID}/webhook', '\App\Http\Aggregates\Start\Controller\StartController@init');
+
+
+
